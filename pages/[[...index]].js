7bbeab11cad9d8734247ebd6f469356bd5052d3f
Kazi Tanvirul Isam Sakib <ktisakib@gmail.com>
2022-09-22 10:51:22 +0000

    sanity setup

---
+++ pages/[[...index]].js
@@ -0,0 +1,29 @@
+import { NextStudio } from "next-sanity/studio";
+import { useEffect, useState } from "react";
+
+import _config from "../sanity.config";
+
+export default function IndexPage() {
+  const [config, setConfig] = useState(_config);
+
+  useEffect(
+    () =>
+      void import(
+        /* webpackIgnore: true */ "https://themer.sanity.build/api/hues?preset=tw-cyan"
+      ).then(({ theme }) => setConfig((config) => ({ ...config, theme }))),
+    [],
+  );
+
+  return (
+    <NextStudio
+      config={config}
+      unstable__head={
+        <link
+          // Speed up the theme loading significantly
+          rel='modulepreload'
+          href={"https://themer.sanity.build/api/hues?preset=tw-cyan"}
+        />
+      }
+    />
+  );
+}
