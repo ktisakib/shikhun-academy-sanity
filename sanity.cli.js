110abcbef6d6c7a80195485a0712276e1d897a35
Kazi Tanvirul Isam Sakib <ktisakib@gmail.com>
2022-09-27 16:15:27 +0000

    theme added

--- sanity.cli.js
+++ sanity.cli.js
@@ -3,6 +3,14 @@ import {createCliConfig} from 'sanity/cli'
 export default createCliConfig({
   api: {
     projectId: 'hi8cvush',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
+  vite: (config) => ({
+    ...config,
+    build: {
+      ...config.build,
+      // esbuild requires es2022 or later to allow top-level await: https://esbuild.github.io/content-types/#javascript
+      target: 'es2022',
+    },
+  }),
 })
--- sanity.config.js
+++ sanity.config.js
@@ -1,15 +1,14 @@
 import {createConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {schemaTypes} from './schemas'
+const {theme} = await import('https://themer.sanity.build/api/hues?preset=tw-cyan')
 export default createConfig({
+  theme,
   name: 'default',
   title: 'shikhun-academy',
-
   projectId: 'hi8cvush',
   dataset: 'production',
-
   plugins: [deskTool()],
-
   schema: {
     types: schemaTypes,
   },
